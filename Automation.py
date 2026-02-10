d4359258efa0daabe799881eb00bef013bf0520b
quqierr <coki020412@gmail.com>
2026-02-10 10:30:13 +0100

    Increase logo image width from 150 to 200

--- Automation.py
+++ Automation.py
@@ -5,7 +5,7 @@ from collections import Counter
 col1, col2 = st.columns([1, 6])
 
 with col1:
-    st.image("Logo.png", width=150)
+    st.image("Logo.png", width=200)
 
 with col2:
     st.markdown(
